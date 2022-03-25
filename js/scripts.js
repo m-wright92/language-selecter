986c04bceb4b176b62b8a105751cfa252428cfc4
Michael Wright <m.wright92@icloud.com>
2022-03-25 16:40:21 -0700

    WIP attempt to allow page to progress after collecting img value

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,75 @@
 $(document).ready(function() {
-  const name = $("input#users-name").val().trim();
-  const color = $("select#question2").val();
+  $(".images").submit(function(event){
+    event.preventDefault();
+    const name = $("input#users-name").val().trim();
+    const color = $("select#question2").val();
 
-  $("#begin").click(function() {
-    $(".intro").addClass("hidden");
-    $(".name").removeClass("hidden");
-  })
-  
-  $("#q1").click(function() {
-    $(".name").addClass("hidden");
-    $(".colors").removeClass("hidden");
-  })
+    const img = ""
+      
 
-  $("#q2").click(function() {
-    $(".colors").addClass("hidden");
-    $(".food").removeClass("hidden");
-  })
 
-  $("#q3").click(function() {
-    $(".food").addClass("hidden");
-    $(".flavor").removeClass("hidden");
-  })
+    $("#begin").click(function() {
+      $(".intro").addClass("hidden");
+      $(".name").removeClass("hidden");
+    })
+    
+    $("#q1").click(function() {
+      $(".name").addClass("hidden");
+      $(".colors").removeClass("hidden");
+    })
 
-  $("#q4").click(function() {
-    $(".flavor").addClass("hidden");
-    $(".vacation").removeClass("hidden");
-  })
+    $("#q2").click(function() {
+      $(".colors").addClass("hidden");
+      $(".food").removeClass("hidden");
+    })
 
-  $("#q5").click(function() {
-    $(".vacation").addClass("hidden");
-    $(".images").removeClass("hidden");
-  })
+    $("#q3").click(function() {
+      $(".food").addClass("hidden");
+      $(".flavor").removeClass("hidden");
+    })
 
-  $(function() {
-    $(".images").click(function() {
-      let fired_button = $("button").val();
-      alert(fired_button);
-    // $(".images").addClass("hidden");
-    // $(".results").removeClass("hidden");
+    $("#q4").click(function() {
+      $(".flavor").addClass("hidden");
+      $(".vacation").removeClass("hidden");
+    })
+
+    $("#q5").click(function() {
+      $(".vacation").addClass("hidden");
+      $(".images").removeClass("hidden");
     });
   });
 
-
+  function imageSelect() {
+    if (this === $("#img1")) {
+      let img = "apple";
+    } else if (this === $("#img2")) {
+      let img = "microsoft";
+    }
+  }
 });
+
+  
+  // $("button#img1").click(function() {
+  //     let fired_button = $("#img1").val();
+  //     alert(fired_button);
+  //   });
+  
+  // $("button#img2").click(function() {
+  //     let fired_button = $("#img2").val();
+  //     alert(fired_button);
+  //   });
+
+  // $("button#img3").click(function() {
+  //     let fired_button = $("#img3").val();
+  //     alert(fired_button);
+  //   });
+
+  // $("button#img4").click(function() {
+  //     let fired_button = $("#img4").val();
+  //     alert(fired_button);
+  //   });
+
+
 
 
 
