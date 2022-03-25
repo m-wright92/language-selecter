70d91c8f68b725f5d35adeefce60f964f11d5d46
Michael Wright <m.wright92@icloud.com>
2022-03-25 11:24:51 -0700

    complete first 3 slide functionaly with select options, WIP on radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,29 @@
 $(document).ready(function() {
+  const name = $("input#users-name").val().trim();
+  const color = $("select#question2").val();
+
   $("#begin").click(function() {
     $(".intro").addClass("hidden");
-    $(".colors").removeClass("hidden");
+    $(".name").removeClass("hidden");
   })
   
   $("#q1").click(function() {
-    $(".colors").addClass("hidden");
+    $(".name").addClass("hidden");
+    $(".colors").removeClass("hidden");
   })
+
+  $("#q2").click(function() {
+    $(".colors").addClass("hidden");
+    $(".food").removeClass("hidden");
+  })
+
+  $("#q3").click(function() {
+    $(".food").addClass("hidden");
+    $("").removeClass("hidden");
+  })
+
+
 })
+
+// for finding value of users name
+// const name = $("input#users-name").val();
